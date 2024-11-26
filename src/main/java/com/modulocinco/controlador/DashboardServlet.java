6aff36d0ef1823265edadd27a6f69ee7278cf7f2
Erick Rivera <yomellamoerick.771@gmail.com>
2024-11-25 21:48:11 -0300

    dashboard y logout

---
+++ src/main/java/com/modulocinco/controlador/DashboardServlet.java
@@ -0,0 +1,54 @@
+package com.modulocinco.controlador;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class DashboardServlet
+ */
+@WebServlet("/dashboard")
+public class DashboardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DashboardServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession(false);
+
+		if (session != null && session.getAttribute("usuario") != null) {
+			response.setContentType("text/html");
+			response.getWriter().println("<h1>Dashboard</h1>");
+			response.getWriter().println("<br>");
+			response.getWriter().println("<h2>Bienvenid@: " + session.getAttribute("usuario") + "</h2>");
+			response.getWriter().println("<a href=\""+ request.getContextPath()+"/dashboard" +"\">Cerrar sesion</a>");
+		} else {
+			response.getWriter().println("<h1>ACCESO DENEGADO.</h1>");
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
