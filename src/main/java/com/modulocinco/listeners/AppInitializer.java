102d57f680da441fdca60c500d8ac5f594587ad2
Erick Rivera <yomellamoerick.771@gmail.com>
2024-11-26 21:13:16 -0300

    init listo

--- src/main/java/com/modulocinco/listeners/AppInitializer.java
+++ src/main/java/com/modulocinco/listeners/AppInitializer.java
@@ -1,5 +1,10 @@
 package com.modulocinco.listeners;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.modulocinco.modelo.Usuario;
+
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
@@ -25,7 +30,16 @@ public class AppInitializer implements ServletContextListener {
      * @see ServletContextListener#contextInitialized(ServletContextEvent)
      */
     public void contextInitialized(ServletContextEvent sce)  { 
-        // Usaremos context
+        
+        // Usaremos context, hacemos la lista
+        List<Usuario> usuarios = new ArrayList<>();
+
+        // Creamos desde el inicio un superadministrador
+        usuarios.add(new Usuario("Administrador", "admin", "1234"));
+        
+        // Compartimos la lista, guardandola en el contexto de la app
+        sce.getServletContext().setAttribute("usuarios", usuarios);
+
     }
 
 	/**
